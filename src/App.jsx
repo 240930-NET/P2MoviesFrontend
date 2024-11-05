cb332994ba9eac3ac0b5f6005136c4775f216114
Gorawto41 <Gorawto42@yahoo.com>
2024-11-05 15:17:23 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Search from './Search.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Your favorite movie's!</h1>
+      <nav>
+        <ul>SEARCH</ul>
+        <ul>PROFILE</ul>
+        <ul>FAVORITES</ul>
+      </nav>
+      <Search></Search>
+    </>
+  )
+}
+
+export default App
---
+++ src/Search.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './Search.css'
+
+function Search(){
+    function handleSearch(e) {
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        console.log(formJson);
+    }
+
+    return (
+        <div>
+            <label>
+                <form method="post" onSubmit={handleSearch}>
+                    Search: <input name='Search' defaultValue={'Howl\'s moving castle'}/>
+                    <button type='submit'>Submit</button>
+                </form>
+            </label>
+        </div>
+    )
+}
+
+export default Search;
